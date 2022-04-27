cf220e6e311b9c13933d15db0ea4ba446736a187
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-04-27 06:11:25 +0200

    install laravel-vue-i18n

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
+import { i18nVue } from 'laravel-vue-i18n'
 
 InertiaProgress.init();
 
@@ -11,6 +12,9 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(i18nVue, {
+                resolve: lang => import(`../../lang/${lang}.json`),
+            })
             .mixin({ methods : {route} })
             .mount(el);
     },
