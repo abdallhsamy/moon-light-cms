dbd99dec5a8c009c3bbba7fe893e31f30f177385
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-04-27 22:37:54 +0200

    translate users index

--- resources/js/Pages/users/Index.vue
+++ resources/js/Pages/users/Index.vue
@@ -4,12 +4,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">{{ $t('Starter Page') }}</h1>
+                        <h1 class="m-0">{{ $t('users_header_title') }}</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">{{ $t('Home') }}</a></li>
-                            <li class="breadcrumb-item active">{{ $t('Starter Page') }}</li>
+                            <li class="breadcrumb-item"><a href="#">{{ $t('breadcrumb_dashboard') }}</a></li>
+                            <li class="breadcrumb-item active">{{ $t('breadcrumb_user_management') }}</li>
                         </ol>
                     </div>
                 </div>
@@ -22,10 +22,9 @@
                     <div class="col-lg-12">
                         <div class="card card-primary card-outline">
                             <div class="card-header">
-                                <h5 class="card-title">Users List</h5>
+                                <h5 class="card-title">{{ $t('users_header_title') }}</h5>
                                 <p class="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the card's
-                                    content.
+                                    {{ $t('users_header_sub_title') }}
                                 </p>
                             </div>
                             <div class="card-body">
@@ -38,7 +37,7 @@
                                     <tr>
                                         <th @click="sort('id')" style="width: 10px">#</th>
                                         <th @click="sort('name')">
-                                            Name
+                                            {{ $t('users_table_name') }}
                                             <svg style="width:12px; height:12px;" v-if="params.field === 'name' && params.direction === 'asc'" xmlns="http://www.w3.org/2000/svg" class="float-right" viewBox="0 0 20 20" fill="currentColor">
                                                 <path d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h5a1 1 0 000-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM13 16a1 1 0 102 0v-5.586l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 101.414 1.414L13 10.414V16z"/>
                                             </svg>
@@ -48,7 +47,7 @@
                                             </svg>
                                         </th>
                                         <th @click="sort('email')">
-                                            Email
+                                            {{ $t('users_table_email') }}
                                             <svg style="width:12px; height:12px;" v-if="params.field === 'email' && params.direction === 'asc'" xmlns="http://www.w3.org/2000/svg" class="float-right" viewBox="0 0 20 20" fill="currentColor">
                                                 <path d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h5a1 1 0 000-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM13 16a1 1 0 102 0v-5.586l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 101.414 1.414L13 10.414V16z"/>
                                             </svg>
@@ -57,7 +56,7 @@
                                                 <path d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h7a1 1 0 100-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM15 8a1 1 0 10-2 0v5.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L15 13.586V8z"/>
                                             </svg>
                                         </th>
-                                        <th style="width: 40px">Verified</th>
+                                        <th style="width: 40px">{{ $t('users_table_verified') }}</th>
                                     </tr>
                                     </thead>
                                     <tbody>
@@ -67,7 +66,10 @@
                                         <td>{{  user.name }}</td>
                                         <td>{{  user.email }}</td>
                                         <td>
-                                            <span class="badge " :class="{ 'bg-success' : user.email_verified_at ,'bg-danger' : !user.email_verified_at }">{{  user.email_verified_at ? 'yes' : 'no' }}</span>
+                                            <span class="badge "
+                                                  :class="{ 'bg-success' : user.email_verified_at ,'bg-danger' : !user.email_verified_at }">
+                                                {{  user.email_verified_at ? $t('boolean_yes') : $t('boolean_no') }}
+                                            </span>
                                         </td>
                                     </tr>
                                     </tbody>
@@ -92,9 +94,6 @@
 import { Link } from "@inertiajs/inertia-vue3";
 import DashboardLayout from "@/Layouts/DashboardLayout";
 import Pagination from '@/Components/Pagination'
-
-// import AppLayout from '@/Layouts/AppLayout';
-// import Pagination from '../Jetstream/Pagination';
 import { pickBy, throttle } from 'lodash';
 
 export default {
