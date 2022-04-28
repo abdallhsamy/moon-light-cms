5776f015aa37faf0c27c1042cb78a08286cdb36e
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-04-28 02:27:06 +0200

    flash messages

--- resources/js/Layouts/DashboardLayout.vue
+++ resources/js/Layouts/DashboardLayout.vue
@@ -5,6 +5,11 @@
         <MainSidebar/>
 
         <div class="content-wrapper">
+<!--            <MainSidebar/>-->
+
+            <div v-if="$page.props.flash.message" class="alert alert-primary" role="alert">
+                {{  $page.props.flash.message }}
+            </div>
 
             <slot/>
 
@@ -18,17 +23,19 @@
 
 <script>
 import ControlSidebar from "./ControlSidebar";
-import MainSidebar from "./MainSidebar";
-import MainHeader from "./MainHeader";
+import FlashMessages from "./FlashMessages";
 import MainFooter from "./MainFooter";
+import MainHeader from "./MainHeader";
+import MainSidebar from "./MainSidebar";
 
 export default {
     name: "DashboardLayout",
     components: {
+        ControlSidebar,
+        FlashMessages,
         MainFooter,
         MainHeader,
         MainSidebar,
-        ControlSidebar
     }
 }
 </script>
---
+++ resources/js/Layouts/FlashMessages.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="$page.props.flash.message" class="alert alert-primary" role="alert">
+        {{  $page.props.flash.message }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FlashMessages"
+}
+</script>
+
+<style scoped>
+
+</style>
