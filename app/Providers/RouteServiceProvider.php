693d0d75c2b1abbb347ae2257c5cf1dcd6c38101
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-04-30 05:48:51 +0200

    separate auth routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,8 +33,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+            Route::middleware('web')->group(base_path('routes/web.php'));
+
+            Route::middleware('web')->group(base_path('routes/auth.php'));
         });
     }
 
